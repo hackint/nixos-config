e7a0eca9a4608ab05c05d9a82a2bc4ea585ba727
Martin Weinelt <hexa@darmstadt.ccc.de>
2021-06-26 12:55:53 +0200

    roles/ircd: configure server connection class

--- config/roles/ircd.nix
+++ config/roles/ircd.nix
@@ -3,10 +3,20 @@
   hackint = {
     solanum = {
       enable = true;
+      isHub = lib.mkDefault false;
       exempts = [
         # TODO: add monitoring
       ];
       opers = {
+      };
+      classes = {
+        server = {
+          pingTime = "5 minutes";
+          autoconnFreq = "1 minute";
+          maxConnections = 16;
+          maxAutoconn = if config.hackint.solanum.isHub then 0 else 1;
+          sendQ = "2 megabytes";
+        };
       };
     };
   };
