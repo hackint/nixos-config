b91fcac937e8eacf2e5e8121a1e7118d7911b8ca
Martin Weinelt <hexa@darmstadt.ccc.de>
2022-08-11 16:39:40 +0200

    machines: add network config to staging hosts

--- config/machines/hub.staging.hackint.org/default.nix
+++ config/machines/hub.staging.hackint.org/default.nix
@@ -9,6 +9,18 @@
     hostName = "hub";
   };
 
+  hackint.network = {
+    macAddress = "00:00:00:11:11:11";
+    addresses = [
+      "192.0.2.2/24"
+      "2001:DB8::1/64"
+    ];
+    gateways = [
+      "192.0.2.1"
+      "fe80::1"
+    ];
+  };
+
   hackint.solanum.sid = "100";
 
   system.stateVersion = "21.05";
--- config/machines/leaf1.staging.hackint.org/default.nix
+++ config/machines/leaf1.staging.hackint.org/default.nix
@@ -9,6 +9,18 @@
     hostName = "leaf1";
   };
 
+  hackint.network = {
+    macAddress = "00:00:00:11:11:12";
+    addresses = [
+      "192.0.2.3/24"
+      "2001:DB8::2/64"
+    ];
+    gateways = [
+      "192.0.2.1"
+      "fe80::1"
+    ];
+  };
+
   hackint.solanum.sid = "200";
 
   system.stateVersion = "21.05";
